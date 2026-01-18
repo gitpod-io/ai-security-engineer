03b50429053f3d255a4737e1a76116462f8b44bd
Moritz Eysholdt <moritz@gitpod.io>
2026-01-18 13:38:08 +0000

    added deploy script

---
+++ deploy-to-ona.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gitpod ai automation update 019bd062-8571-7958-b6e1-898cabbe67a3 fix.yaml 
+gitpod ai automation update 019bcc1d-eb6f-7a82-9959-2c776118fa38 feed.yaml 
+gitpod ai automation update 019bcbfc-af85-7a44-bf7a-3b107298127b scan.yaml 
