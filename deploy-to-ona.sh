1ac783017c1bc017a4e7ffd310ff1e7466d98d2e
Moritz Eysholdt <moritz@gitpod.io>
2026-01-20 16:24:38 +0000

    added demo
    Co-authored-by: Ona <no-reply@ona.com>

--- deploy-to-ona.sh
+++ deploy-to-ona.sh
@@ -4,3 +4,4 @@ gitpod ai automation update 019bd062-8571-7958-b6e1-898cabbe67a3 fix.yaml
 gitpod ai automation update 019bcc1d-eb6f-7a82-9959-2c776118fa38 feed.yaml 
 gitpod ai automation update 019bcbfc-af85-7a44-bf7a-3b107298127b scan.yaml 
 gitpod ai automation update 019bd6ae-0f52-7c6b-9d6f-0c8eaebbf8b0 drive.yaml
+gitpod ai automation update 019bdc32-9e16-7361-a282-d9c319a93bc1 demo.yaml
